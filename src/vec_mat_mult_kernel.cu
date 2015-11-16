4745e03e2679bc271e0abfae73cc97391e11912f
Darius Remeika <Rdariux@gmail.com>
2015-11-15 19:50:52 -0500

    Initial commit.

---
+++ src/vec_mat_mult_kernel.cu
@@ -0,0 +1,23 @@
+/* Vector-Matrix multiplication: Y = A * X.
+ * Device code.
+ */
+
+#ifndef _MATRIXMUL_KERNEL_H_
+#define _MATRIXMUL_KERNEL_H_
+
+#include "vec_mat_mult.h"
+
+__global__ void vec_mat_kernel_naive(float *Ad, float *Xd, float *Yd)
+{
+	//Multiply A and X
+}
+
+
+__global__ void vec_mat_kernel_optimized(float *Ad, float *Xd, float *Yd)
+{
+	//Multiply A and X
+}
+
+
+
+#endif // #ifndef _MATRIXMUL_KERNEL_H_
